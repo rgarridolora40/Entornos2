c2ce343ecf60398010f50dcda2708a89e57ed0b7
studium <studium@DESKTOP-CGGB52H>
2018-03-11 11:52:48 +0100

    mi primer commit ?

---
+++ contadornumeros/src/es/studium/contadornumeros/contadornumeros.java
@@ -0,0 +1,21 @@
+package es.studium.contadornumeros;
+
+import java.util.Random;
+public class contadornumeros
+{
+	static Random rnd=new Random();
+	public static void main(String[] args)
+	{
+		int veces;
+		int contador=0;
+		veces = rnd.nextInt(100)+1;
+		for(int i = 0; i<veces;i++)
+		{
+			contador = 0;
+			System.out.println(rnd.nextInt(50));
+			contador++;
+		}
+		System.out.println("Se han generado "+contador+" números aleatorios");
+	}
+
+}
